81822b5b28640dd9adfe56030798573b7326e8aa
jasehjc <161805028+jasehjc@users.noreply.github.com>
2026-09-04 18:11:24 +0800

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='koko-v3-3';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='koko-v3-4-icon';const A=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png',
+'./icon-192.png',
+'./icon-512.png',
+'./favicon-32.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
