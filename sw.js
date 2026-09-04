4a3cd649433084b7c5c89f87298924deecce8008
jasehjc <161805028+jasehjc@users.noreply.github.com>
2026-09-04 18:19:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,53 @@
-const C='koko-v3-4-icon';const A=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png',
-'./icon-192.png',
-'./icon-512.png',
-'./favicon-32.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='koko-v3-3-1-icon-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './coco-icon-180-v1.png',
+  './coco-icon-192-v1.png',
+  './coco-icon-512-v1.png',
+  './coco-favicon-32-v1.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(cache => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        if (req.method === 'GET' && res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then(cache => cache.put(req, copy));
+        }
+        return res;
+      });
+    })
+  );
+});
